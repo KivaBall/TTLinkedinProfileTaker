8a338fee2744b8a72f550be52e7ed83156949022
Ihor Kobzar <supermensy@gmail.com>
2025-02-22 11:52:14 +0200

    Add entity request for endpoint

---
+++ TTLinkedinProfileTaker/Entities/ProfilePhotoRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TTLinkedinProfileTaker.Entities;
+
+public sealed class ProfilePhotoRequest
+{
+    public required string Login { get; init; }
+    public required string Password { get; init; }
+}
